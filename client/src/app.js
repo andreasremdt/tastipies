53a097c3da41b7a3c8494231b5a2c744129c5d49
Andreas Remdt <andreas.remdt@gmail.com>
2018-09-25 14:24:04 +0200

    add login page

--- client/src/app.js
+++ client/src/app.js
@@ -3,8 +3,11 @@ import { render } from "react-dom";
 import NavBar from "./components/NavBar";
 import HomePage from "./components/HomePage";
 import RecipeList from "./components/RecipeList";
+import LoginPage from "./components/LoginPage";
 import "./styles.scss";
 import { Router } from "@reach/router";
+import SignUpPage from "./components/SignUpPage";
+import ForgotPasswordPage from "./components/ForgotPasswordPage";
 
 const App = () => {
   return (
@@ -14,6 +17,9 @@ const App = () => {
       <Router>
         <HomePage path="/" />
         <RecipeList path="/recipes" />
+        <LoginPage path="/login" />
+        <SignUpPage path="/register" />
+        <ForgotPasswordPage path="/forgot-password" />
       </Router>
     </React.Fragment>
   );
