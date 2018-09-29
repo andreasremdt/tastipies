751ec142d7f3b9402b48a23c8714d03c2f9ecdb1
Andreas Remdt <andreas.remdt@gmail.com>
2018-09-29 15:54:40 +0200

    add recipe controller and model

--- server/controllers/recipe.js
+++ server/controllers/recipe.js
@@ -1,81 +1,59 @@
-var _ = require('lodash');
-var { ObjectId } = require('mongodb');
-var User = require('../models/user');
-var User = require('../models/recipe');
+const _ = require("lodash");
+const Recipe = require("../models/recipe");
+const { handleError } = require("../helpers");
 
-require('../database/connect');
-
-module.exports = class UserController {
+class RecipeController {
   static index(req, res) {
-    // User.find({})
-    //   .then(users => res.json(users))
-    //   .catch(error => res.status(500).json({ message: 'Could not display users.', error }));
+    Recipe.find({})
+      .then(recipes => res.json(recipes))
+      .catch(err => res.status(500).json(err));
   }
 
   static show(req, res) {
-    if (!ObjectId.isValid(req.params.id)) {
-      return res.status(400).json({ message: 'The given ID is not valid.' });
-    }
+    Recipe.findById(req.params.id)
+      .then(recipe => {
+        if (!recipe) return res.status(404).send();
 
-    User.findById(req.params.id).then(user => {
-      if (!user) {
-        res.status(404).json({ message: 'User could not be found.' });
-      } else {
-        res.json(user);
-      }
-    }).catch(error => res.status(500).json({ message: 'User could not be retrieved.', error }));
+        res.json(recipe);
+      })
+      .catch(err => res.status(500).json(err));
   }
 
-  static store(req, res, next) {
-    Recipe.create(req.body)
-      .then(user => res.json(user))
-      .catch(err => console.error(err));
-    /*User.create(_.merge(_.pick(req.body, ['firstName', 'lastName', 'username', 'password', 'email', 'active']), { created: Date.now() }))
-      .then(user => res.json({ message: 'New user has been created successfully', user }))
-      .catch(error => {
-        if (error.code == 11000) {
-          if (error.message.includes('$username')) {
-            res.status(400).json({ message: 'The username is already taken. Please choose another username.' });
-          } else if (error.message.includes('$email')) {
-            res.status(400).json({ message: 'The email address is already taken. Please choose another email address.' });
-          }
-        } else {
-          res.status(400).json({ message: 'The validation has failed. Please correct your input.', error: error.errors });
-        }
-      });*/
+  static store(req, res) {
+    const body = _.merge(_.pick(req.body, Object.keys(Recipe.schema.paths)), {
+      _created: Date.now()
+    });
+
+    Recipe.create(body)
+      .then(() => res.status(201).send())
+      .catch(err => handleError(err, res));
   }
 
   static update(req, res) {
-    if (!ObjectId.isValid(req.params.id)) {
-      return res.status(400).json({ message: 'The given ID is not valid.' });
-    }
+    const changes = _.pick(req.body, Object.keys(Recipe.schema.paths));
 
-    User.findByIdAndUpdate(req.params.id, { $set: _.pick(req.body, 'firstName', 'lastName', 'password', 'email', 'username', 'active') }, { runValidators: true }).then(user => {
-      if (!user) {
-        res.status(404).json({ message: 'No user found with the given ID.' });
-      } else {
-        res.json({ message: 'User has been updated successfully.', user })
-      }
-    }).catch(error => {
-      if (error.code == 11000) {
-        res.status(400).json({ message: 'The email address is already taken. Please choose another email address.' });
-      } else {
-        res.status(400).json({ message: 'The validation has failed. Please correct your input.', error: error.errors });
-      }
-    });
+    Recipe.findByIdAndUpdate(
+      req.params.id,
+      { $set: changes },
+      { runValidators: true }
+    )
+      .then(recipe => {
+        if (!recipe) return res.status(404).send();
+
+        res.status(204).send();
+      })
+      .catch(err => handleError(err, res));
   }
 
   static destroy(req, res) {
-    if (!ObjectId.isValid(req.params.id)) {
-      return res.status(400).json({ message: 'The given ID is not valid.' });
-    }
+    Recipe.findByIdAndDelete(req.params.id)
+      .then(recipe => {
+        if (!recipe) return res.status(404).send();
 
-    User.findByIdAndRemove(req.params.id).then(user => {
-      if (!user) {
-        res.status(404).json({ message: 'No user found with the given ID.' });
-      } else {
-        res.json({ message: 'User has been deleted successfully.', user })
-      }
-    }).catch(error => res.status(500).json({ message: 'User could not be deleted.', error }));
+        res.status(204).send();
+      })
+      .catch(err => res.status(500).json(err));
   }
 }
+
+module.exports = RecipeController;
--- server/models/recipe.js
+++ server/models/recipe.js
@@ -1,24 +1,60 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
 var recipeSchema = new mongoose.Schema({
   title: {
     type: String,
-    required: [true, 'The recipe title is required.'],
+    required: [true, "The recipe title is required."],
     minlength: 4,
     maxlength: 255
   },
+  photo: {
+    type: String,
+    required: [true, "The recipe photo is required."]
+  },
+  video: String,
+  difficulty: {
+    type: String,
+    required: [true, "The difficulty level is required."],
+    enum: ["Easy", "Medium", "Difficult"]
+  },
+  category: {
+    type: String,
+    required: [true, "The recipe category is required."]
+  },
+  cooking_method: {
+    type: String,
+    required: [true, "The cooking method is required."]
+  },
+  cuisine: String,
+  short_description: String,
+  prep_time: Number,
+  cook_time: Number,
+  ingredients: [
+    {
+      type: String
+    }
+  ],
+  instructions: [
+    {
+      type: String
+    }
+  ],
+  notes: String,
+  yields: {
+    type: String,
+    required: [true, "The amount of servings is required."]
+  },
   _author: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
+    ref: "User"
   },
-  body: {
-    type: String,
-    required: [true, 'The recipe body is required.']
+  _created: {
+    type: Date
   },
-  date: {
+  _updated: {
     type: Date,
-    default: Date.now
+    default: Date.now()
   }
 });
 
-module.exports = Recipe = mongoose.model('Recipe', recipeSchema);
+module.exports = Recipe = mongoose.model("Recipe", recipeSchema);
